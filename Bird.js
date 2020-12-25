3e9484d07b84dd33ff5792919471550baa546766
burhanuddin5150 <burhanuddin.mg@gmail.com>
2020-12-25 15:49:48 +0530

    changed bird.js

--- Bird.js
+++ Bird.js
@@ -2,6 +2,10 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.somkeImage = loadImage("sprites/smoke.png")
+    this.trajectory = [];
+
+
   }
 
   display() {
@@ -9,5 +13,13 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if (this.body.velocity.x>10 && this.body.position.x >200){
+    var position = [this.body.position.x,this.body.position.y]
+    this.trajectory.push(position)
+    }
+
+    for ( var i =0;i<this.trajectory.length ;i++){
+      image(this.somkeImage,this.trajectory[i][0],this.trajectory[i][1]);
+    }
   }
 }
